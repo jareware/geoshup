8020a975f5a2c83a77183ca251e716ae81186725
Jarno Rantanen <jarno@jrw.fi>
2012-08-02 21:08:21 +0300

    Improved the logging facilities a bit.

--- tests/spec/utils/logger.js
+++ tests/spec/utils/logger.js
@@ -1,6 +1,7 @@
 define([
+    'lib/moment',
     'utils/logger'
-], function(logger) {
+], function(moment, logger) {
 
     describe('utils/logger', function() {
 
@@ -11,6 +12,10 @@ define([
             orig = logger.getTopLabelLength(); // initializations for previous tests may have affected the prefix length counter
 
             logger.setTopLabelLength(0);
+
+            spyOn(moment.fn, 'format').andCallFake(function() {
+                return '12:34';
+            });
 
         });
 
@@ -31,8 +36,24 @@ define([
             log2('more', 123, 'testing');
 
             expect(window.console.log.toHaveBeenCalled);
-            expect(window.console.log.argsForCall[0]).toEqual(['   [foo]', 'testing']);
-            expect(window.console.log.argsForCall[1]).toEqual(['[barbaz]', 'more', 123, 'testing']);
+            expect(window.console.log.argsForCall[0]).toEqual(['12:34    [foo]', 'testing']);
+            expect(window.console.log.argsForCall[1]).toEqual(['12:34 [barbaz]', 'more', 123, 'testing']);
+
+        });
+
+        it('logs hashes nicely', function() {
+
+            var log = logger.create('foo');
+
+            spyOn(window.console, 'log');
+
+            log.hash({
+                foo: 'bar',
+                baz: 123
+            });
+
+            expect(window.console.log).toHaveBeenCalled();
+            expect(window.console.log.argsForCall[0]).toEqual(['12:34 [foo]', 'foo:', 'bar', ', ', 'baz:', 123]);
 
         });
 
--- utils/logger.js
+++ utils/logger.js
@@ -1,6 +1,13 @@
-define(function() {
+define([
+    'lib/underscore',
+    'lib/moment'
+], function(_, moment) {
 
     "use strict";
+
+    var ENABLED = true;
+    var TIMESTAMP = true;
+    var FORMAT = '@ss.SSS'; // 'HH:mm:ss:SSS';
 
     var topLabelLength = 0;
 
@@ -20,6 +27,9 @@ define(function() {
 
         create: function(label) {
 
+            if (!ENABLED)
+                return function() {};
+
             if (label.length > topLabelLength)
                 topLabelLength = label.length;
 
@@ -28,14 +38,31 @@ define(function() {
                 if (!window.console || !window.console.log)
                     return;
 
+                var timestamp = TIMESTAMP ? moment().format(FORMAT) + ' ' : '';
                 var tagPadding = new Array(topLabelLength - label.length + 1).join(' ');
                 var argsToLog  = Array.prototype.slice.call(arguments);
 
-                window.console.log.apply(window.console, [ tagPadding + logger.label ].concat(argsToLog));
+                window.console.log.apply(window.console, [ timestamp + tagPadding + logger.label ].concat(argsToLog));
 
             };
 
             logger.label = '[' + label + ']';
+
+            logger.hash = function(hash) {
+
+                var pieces = [];
+
+                _.each(hash, function(value, key) {
+                    pieces.push(key + ':');
+                    pieces.push(value);
+                    pieces.push(', ');
+                });
+
+                pieces.pop();
+
+                logger.apply(logger, pieces);
+
+            };
 
             return logger;
 
